9a62b91d0e87df2193d86804ea8e002fba9d1d0b
dcalligy <dcalligy813@gmail.com>
2022-04-07 20:57:23 -0400

    Adding comments to palindrome script

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,19 @@
-var isPalindrome = function(x) {
+// Given an integer x, return true if x is a palindrome integer.
+// An integer is a palindrome when it read the same backwards as forward.
+// For example, 121 is a palindrome when it reads the same backwards as forward.
+// Example 1:
+// Input: 1
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+/**
+ * @param {number} x
+ * @return {boolean}
+ *
+ */
+ var isPalindrome = function(x) {
   let str = x.toString();
   let start = 0;
   let end = str.length - 1;
-  console.log('end: ', end);
   while (start < end) {
     if (str.charAt(start++) != str.charAt(end--)) {
       return false;
@@ -11,7 +22,8 @@ var isPalindrome = function(x) {
   return true;
 };
 
-console.log('palindrome: ', isPalindrome(121));
-console.log('palindrome: ', isPalindrome(1221));
-console.log('palindrome: ', isPalindrome(11));
-console.log('palindrome: ', isPalindrome(32323));
+console.log(isPalindrome(121));
+console.log(isPalindrome(1221));
+console.log(isPalindrome(2202));
+console.log(isPalindrome(11));
+console.log(isPalindrome(391023));
