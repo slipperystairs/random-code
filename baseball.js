39f93784c964bcad2682473f25ad3137e6ae08b5
dcalligy <dcalligy813@gmail.com>
2022-04-11 20:51:09 -0400

    Adding the example comments and logging

--- baseball.js
+++ baseball.js
@@ -12,6 +12,36 @@
 // 4. "C" - Invalidate the previous score, removing it from the record. It is guaranteed
 // there will always be a previous score.
 
+/* Example 1:
+* Input: ops = ["5","2","C","D","+"]
+* Output: 30
+* Explanation:
+* "5" - Add 5 to the record, record is now [5].
+* "2" - Add 2 to the record, record is now [5, 2].
+* "C" - Invalidate and remove the previous score, record is now [5].
+* "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+* "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+* The total sum is 5 + 10 + 15 = 30.
+*
+* Example 2:
+* Input: ops = ["5","-2","4","C","D","9","+","+"]
+* Output: 27
+* Explanation:
+* "5" - Add 5 to the record, record is now [5].
+* "-2" - Add -2 to the record, record is now [5, -2].
+* "4" - Add 4 to the record, record is now [5, -2, 4].
+* "C" - Invalidate and remove the previous score, record is now [5, -2].
+* "D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+* "9" - Add 9 to the record, record is now [5, -2, -4, 9].
+* "+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+* "+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+* The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+* 
+* Example 3:
+* Input: ops = ["1"]
+* Output: 1
+*/
+
 /**
  * @param {string[]} ops
  * @return {number}
@@ -25,6 +55,7 @@ var calPoints = function(ops) {
   let prev_score = [];
   let actual_score = [];
   for (let i = 0; i < ops.length; i++) {
+    console.log('ops is: ', ops[i]);
     if (!isNaN(parseInt(ops[i]))) {
       console.log('ops: ', parseInt(ops[i]));
       prev_score.push(parseInt(ops[i]));
@@ -54,4 +85,3 @@ var calPoints = function(ops) {
 };
 
 calPoints(ops);
-
