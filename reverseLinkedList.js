24b619a852b0b1fe157088fadd2224598d4e91c9
dcalligy <dcalligy813@gmail.com>
2022-06-12 18:25:56 -0400

    Comments

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -8,15 +8,24 @@
 /**
  * @param {ListNode} head
  * @return {ListNode}
+ * Iterate through all nodes to traverse the list as long
+ * as there is a node and do the following at each iteration:
+ *  - Set current to head.next
+ *  - Set head.next to previous
+ *  - Set previous to head
+ *  - Set head to current
+ *  - Repeat for all the nodes
+ *  - Return the previous pointer as the new head of
+ *    reversed list.
  */
  var reverseList = function(head) {
   let current;
   let previous = null;
   while (head != null) {
-    current = head.next; // Set curr to the head
-    head.next = previous; // head is set prev
-    previous = head; // prev is now the head of the list
-    head = current; // Head is now the current node.
+    current = head.next;
+    head.next = previous;
+    previous = head;
+    head = current;
   }
   return previous;
 };
