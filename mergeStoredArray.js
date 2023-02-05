c9f8d0fc3caf92d06d5848f7b28b964c0d7f0faa
dcalligy <dcalligy813@gmail.com>
2023-02-05 17:15:03 -0500

    Starting a new problem

---
+++ mergeStoredArray.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/* #88 Merge sorted array
+*  You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+*  and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+* 
+*  Merge nums1, and nums2 into a single array sorted in non-decreasing order.
+* 
+*  The final sorted array should not be returned by the function, but instead be
+*  stored inside the array nums1. To accommodate this, nums1 has the length of m + n,
+*  where the first m elements denote the elements that should be merged, and the last
+*  n  elements should be ignored. num2 has the length of m.
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = (nums1, m, nums2, n) => {
+};
