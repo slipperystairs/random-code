3c0dc48aec9c63eebb98894cc9c7e5cca54a98f8
Dilly <dcalligy813@gmail.com>
2023-02-20 20:39:25 -0500

    Using an arrow function adding a description to our approach.

--- strings/validParentheses.js
+++ strings/validParentheses.js
@@ -5,7 +5,15 @@
 // * Open brackets must be closed in the correct order.
 // * Every close bracket has a corresponding open bracket of the same type.
 
-function isValid(s) {
+// Approach:
+// * Use a stack of characters.
+// * When we encounter and opening bracket,
+//   push it to the top of the stack..
+// * When we encounter a closing bracket,
+//   check if the top of the stack was the opening bracket.
+// * If yes, pop it from the stack. Otherwise, return false.
+
+const isValid = (s) => {
   const stack = [];
   const legend = {
     '(': ')',
