2304452c425ec62e155cd857a84f58f5e7a51b1b
dcalligy <dcalligy813@gmail.com>
2024-01-18 21:07:11 -0500

    Another one

--- sliding-window/sellStock.js
+++ sliding-window/sellStock.js
@@ -22,6 +22,19 @@
  * @return {number}
  */
 const maxProfit = function(prices) {
+  let profit = 0;
+  let min = prices[0];
+  let max = min;
+  for (let i = 0; i <= prices.length; i++) {
+    if (i != prices.length - 1 && prices[i] <= min) {
+      min = prices[i];
+      max = min;
+    } else if (prices[i] > max) {
+      max = prices[i];
+    }
+    profit = max - min > profit ? max - min : profit;
+  }
+  return profit;
 };
 
 console.log(maxProfit([7,1,5,3,6,4]));
